2e345b499ede2670b7e79dc326a6ff43cc94278e
webstar0316 <webstar0316@outlook.com>
2019-02-11 05:13:15 +1000

    Add AppBar and grid

--- src/App/index.js
+++ src/App/index.js
@@ -3,6 +3,7 @@ import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import styled, { css } from 'styled-components';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 // const MyButton = styled.div`
 //   color: green;
@@ -28,6 +29,7 @@ class App extends Component {
       //   <TomatoButton>Hello</TomatoButton>
       // </div>
       <AppLayout>
+        <AppBar/>
         <WelcomeMessage/>
       </AppLayout>
     )
