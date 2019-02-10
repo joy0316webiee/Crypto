545cd43a34dae45dada4b6a5e9bb57fae87fa05b
webstar0316 <webstar0316@outlook.com>
2019-02-11 04:50:43 +1000

    Google font added

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import styled, { css } from 'styled-components';
+
+const MyButton = styled.div`
+  color: green;
+  ${props =>
+    props.primary &&
+    css`
+      color: palevioletred;
+    `}
+`;
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <WelcomeMessage />
+        <MyButton>Hello</MyButton>
+        <MyButton primary>Hello</MyButton>
+        <TomatoButton>Hello</TomatoButton>
+      </div>
     );
   }
 }
