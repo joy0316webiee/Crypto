b51316c537f18c6be2f93c333fc1a5ee3b74ed13
webstar0316 <webstar0316@outlook.com>
2019-02-11 05:00:05 +1000

    Add App Layout

--- src/App/index.js
+++ src/App/index.js
@@ -2,31 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import styled, { css } from 'styled-components';
+import AppLayout from './AppLayout';
 
-const MyButton = styled.div`
-  color: green;
-  ${props =>
-    props.primary &&
-    css`
-      color: palevioletred;
-    `}
-`;
+// const MyButton = styled.div`
+//   color: green;
+//   ${props =>
+//     props.primary &&
+//     css`
+//       color: palevioletred;
+//     `}
+// `;
 
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
+// const TomatoButton = styled(MyButton)`
+//   color: tomato;
+//   border-color: tomato;
+// `;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <WelcomeMessage />
-        <MyButton>Hello</MyButton>
-        <MyButton primary>Hello</MyButton>
-        <TomatoButton>Hello</TomatoButton>
-      </div>
-    );
+      // <div>
+      //   <WelcomeMessage />
+      //   <MyButton>Hello</MyButton>
+      //   <MyButton primary>Hello</MyButton>
+      //   <TomatoButton>Hello</TomatoButton>
+      // </div>
+      <AppLayout>
+        <WelcomeMessage/>
+      </AppLayout>
+    )
   }
 }
 
